9336e2140565d8512437ba3f32894e50466df088
QIAO <281234086@qq.com>
2021-04-24 03:05:44 +0800

    '冒泡排序'

--- struct/paixu/bubble_sort.py
+++ struct/paixu/bubble_sort.py
@@ -3,10 +3,10 @@
 def bubble_sort(sort_list):
     if len(sort_list) <= 1:
         return sort_list
-    for i in range(len(sort_list)):
-        for j in range(i):
-            if sort_list[j] > sort_list[j+1]:
-                sort_list[j], sort_list[j+1] = sort_list[j+1], sort_list[j]
+    for i in range(len(sort_list)):  # 循环遍历整个列表
+        for j in range(1, len(sort_list)-i):  # 每次遍历进行比较
+            if sort_list[j-1] > sort_list[j]:
+                sort_list[j], sort_list[j-1] = sort_list[j-1], sort_list[j]
 
 
 li = [54,26,93,17,77,31,44,55,20]
